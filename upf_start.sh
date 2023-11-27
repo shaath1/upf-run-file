6c2cb90adce504a1b9f9532eac698f9d1b3d7435
shaath1 <66055039+shaath1@users.noreply.github.com>
2023-11-27 12:35:11 +0400

    Update upf_start.sh

--- upf_start.sh
+++ upf_start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Check if lsmod | grep gtp5g returns empty
+if lsmod | grep -q gtp5g; then
+    echo "Module gtp5g is loaded."
+else
+    echo "Module gtp5g is not loaded. Compiling and installing gtp5g..."
+    cd ~/gtp5g
+    make
+    sudo make install
+fi
 cd /home/ubuntu/free5gc/config
 rm upfcfg.yaml
 curl -O https://raw.githubusercontent.com/shaath1/upf-run-file/main/upfcfg.yaml
